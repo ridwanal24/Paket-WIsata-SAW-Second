5a37ab65c52d0e8382de73472cde0d926e2c9191
ridwanal24 <ridwanalakhyar1999@gmail.com>
2021-03-24 22:37:00 +0700

    validasi input kosong

--- admin/penilaian.php
+++ admin/penilaian.php
@@ -4,15 +4,16 @@
   </div>
   <div class="card-body">
     <div class="table-responsive">
-      <form method="post" action="index.php?page=penilaianhasil">
+      <form method="post" action="index.php?page=penilaianhasil" onsubmit="return penilaianCheck()">
         <?php
         $query = $koneksi->query("SELECT * FROM tb_paketwisata_grup");
         ?>
 
         <div class="row">
-          <div class="col-md">
+          <div class="col">
             <div class="form-group">
               <label>Paket Wisata Tujuan</label>
+              <small class="text-danger paket-kosong">*Pilihan wajib diisi</small>
               <select name="paket_tujuan" class="form-control">
                 <option value=""> -- Pilih Wisata Tujuan -- </option>
 
@@ -33,8 +34,9 @@
           <div class="col-md-4">
             <div class="form-group">
               <label>C1. Harga Paket Wisata</label>
+              <small class="text-danger harga-kosong">*Pilihan wajib diisi</small>
               <select class="form-control" name="hrg">
-                <option value="">--Pilih Harga--</option>
+                <option value=""> -- Pilih Harga -- </option>
                 <?php $ambil2 = $koneksi->query("SELECT * FROM tb_subkriteria where id_kriteria='1' order by id_subkriteria asc;");
                 while ($setiap = $ambil2->fetch_assoc()) {
                 ?>
@@ -46,8 +48,9 @@
           <div class="col-md-4">
             <div class="form-group">
               <label>C2. Jumlah Wisata</label>
+              <small class="text-danger jumlah-kosong">*Pilihan wajib diisi</small>
               <select class="form-control" name="jml_wisata">
-                <option value="">--Pilih Jumlah Wisata--</option>
+                <option value=""> -- Pilih Jumlah Wisata -- </option>
                 <?php $ambil2 = $koneksi->query("SELECT * FROM tb_subkriteria where id_kriteria='3' order by id_subkriteria asc;");
                 while ($setiap = $ambil2->fetch_assoc()) {
                 ?>
@@ -59,8 +62,9 @@
           <div class="col-md-4">
             <div class="form-group">
               <label>C3. Lama Tour</label>
+              <small class="text-danger lama-kosong">*Pilihan wajib diisi</small>
               <select class="form-control" name="lm_tour">
-                <option value="">--Pilih Lama Tour --</option>
+                <option value=""> -- Pilih Lama Tour -- </option>
                 <?php $ambil2 = $koneksi->query("SELECT * FROM tb_subkriteria where id_kriteria='4' order by id_subkriteria asc;");
                 while ($setiap = $ambil2->fetch_assoc()) {
                 ?>
